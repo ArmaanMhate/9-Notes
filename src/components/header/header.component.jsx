5d8784a8b38a9ea063227fd7843b321d003f6c4e
Armaan Mhate <armaanmhate494@gmail.com>
2021-06-24 19:02:38 +0530

    created active functionality in the header using NavLink

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,7 +4,8 @@ import {
     BrowserRouter as Router,
     Route,
     Switch,
-    Link
+    Link,
+    NavLink
 } from 'react-router-dom'
 export default class HeaderComponent extends Component {
     constructor() {
@@ -13,16 +14,16 @@ export default class HeaderComponent extends Component {
 
         }
     }
-
+    //NavLink allows to conditionally render a class. https://reactrouter.com/web/api/NavLink
     render() {
         return (
             <div class="header">
-                <Link to="/" className="logo">9 NOTES</Link>
+                <Link to="/"className="logo">9 NOTES</Link>
                 <div className="items">
-                <Link to="/about" className={`about-us item-1 item `}>About Us</Link>
-                <Link to="/find" className="about-us item-2 item">Get Notes</Link>
-                <Link to="/submit" className="about-us item-3 item">Submit Notes</Link>
-                <Link to="/contributers" className="about-us item-4 item">Contributers</Link>
+                    <NavLink to="/about" activeClassName="active"  className={`about-us item-1 item `}>About Us</NavLink>
+                    <NavLink to="/find" activeClassName="active" className="about-us item-2 item">Get Notes</NavLink>
+                    <NavLink to="/submit" activeClassName="active" className="about-us item-3 item">Submit Notes</NavLink>
+                    <NavLink to="/contributers" activeClassName="active" className="about-us item-4 item">Contributers</NavLink>
                 </div>
             </div>
         )
